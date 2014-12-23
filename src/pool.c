0ff9747a52ca627bc3ebde0aec7339150121a8f0
Chris Dickinson <christopher.s.dickinson@gmail.com>
2014-12-23 01:41:31 -0800

    make mask size configureable-ish

--- src/pool.c
+++ src/pool.c
@@ -16,9 +16,12 @@
 
 **/
 
+#define MASK_SIZE 2
+#define FREE_SIZE (MASK_SIZE * 64)
+
 typedef struct yrc_pool_arena_s {
   struct yrc_pool_arena_s* next;
-  size_t used_mask;
+  size_t used_mask[MASK_SIZE];
   uint_fast16_t free;
   char data[1];
 } yrc_pool_arena_t;
@@ -35,18 +38,19 @@ struct yrc_pool_s {
 static yrc_pool_arena_t* alloc_arena(yrc_pool_t* pool) {
   yrc_pool_arena_t** iter;
   yrc_pool_arena_t* arena;
-  uint_fast8_t i;
-  arena = malloc(sizeof(yrc_pool_arena_t) - 1 + (sizeof(yrc_pool_arena_t**) + pool->objsize) * 64);
+  uint_fast32_t i;
+  arena = malloc(sizeof(yrc_pool_arena_t) - 1 + (sizeof(yrc_pool_arena_t**) + pool->objsize) * FREE_SIZE);
   if (arena == NULL) {
     return NULL;
   }
-  arena->used_mask = 0;
   arena->next = NULL;
-  arena->used_mask = 0xFFFFFFFFFFFFFFFF;
-  arena->free = 64;
+  for(i = 0; i < MASK_SIZE; ++i) {
+    arena->used_mask[i] = 0xFFFFFFFFFFFFFFFF;
+  }
+  arena->free = FREE_SIZE;
   ++pool->num_arenas;
   iter = (yrc_pool_arena_t**)arena->data;
-  for(i = 0; i < 64; ++i) {
+  for(i = 0; i < FREE_SIZE; ++i) {
     *iter = arena;
     iter = (void*)((sizeof(yrc_pool_arena_t**) + pool->objsize) + (size_t)(iter));
   }
@@ -75,12 +79,17 @@ int yrc_pool_init(yrc_pool_t** ptr, size_t objsize) {
 void* yrc_pool_attain(yrc_pool_t* pool) {
   yrc_pool_arena_t* cursor;
   int arena_pos;
+  int i;
 retry:
   if (pool->current->free) {
-    arena_pos = clz(pool->current->used_mask);
-    pool->current->used_mask &= ~(1UL << (63 - arena_pos));
-    --pool->current->free;
-    return (void*)((size_t)pool->current->data + (size_t)(arena_pos * (pool->objsize + sizeof(yrc_pool_arena_t**))));
+    for (i = 0; i < MASK_SIZE; ++i) {
+      if (pool->current->used_mask[i]) {
+        arena_pos = clz(pool->current->used_mask[i]);
+        pool->current->used_mask[i] &= ~(1UL << (63 - arena_pos));
+        --pool->current->free;
+        return (void*)((size_t)pool->current->data + (size_t)((arena_pos + (i<<6)) * (pool->objsize + sizeof(yrc_pool_arena_t**))));
+      }
+    }
   }
   /* if we've seen no deallocations since last time, just skip forward. */
   if (!pool->deallocs) {
@@ -106,8 +115,9 @@ retry:
 int yrc_pool_release(yrc_pool_t* pool, void* ptr) {
   void* baseptr = (void*)((size_t)ptr - sizeof(yrc_pool_arena_t**));
   yrc_pool_arena_t* arena = *(yrc_pool_arena_t**)(baseptr);
-  arena->used_mask |= 1UL << ((uint_fast32_t)((size_t)baseptr - (size_t)arena->data) /
+  int arena_pos = ((uint_fast32_t)((size_t)baseptr - (size_t)arena->data) /
     (sizeof(yrc_pool_arena_t**) + pool->objsize));
+  arena->used_mask[arena_pos >> 6] |= 1UL << (arena_pos & 63);
   ++arena->free;
   pool->deallocs = 1;
   if (arena->free > pool->current->free) {
